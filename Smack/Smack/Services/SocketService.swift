afced088974c4c848c8f4ccc787f84f9bc0b93ed
Roger Florat <rflorat@cidiverte.it>
2018-01-17 15:50:40 +0100

    End of lesson 25.

--- Smack/Smack/Services/SocketService.swift
+++ Smack/Smack/Services/SocketService.swift
@@ -16,7 +16,7 @@ class SocketService: NSObject {
     override init() {
         super.init()
     }
-
+    
     var socket : SocketIOClient = SocketIOClient(socketURL: URL(string: BASE_URL)!)
     
     func establishConnection() {
@@ -47,5 +47,16 @@ class SocketService: NSObject {
             completion(true)
         }
     }
+    
+    func addMessage(messageBody: String, userId: String, channelId: String, completion: @escaping CompletionHandler) {
+        
+        let user = UserDataService.instance
+        
+        socket.emit("newMessage", messageBody, userId, channelId, user.name, user.avatarName, user.avatarColor)
+        
+        completion(true)
+    }
+    
+    
     
 }
