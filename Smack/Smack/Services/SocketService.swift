b80412f525679205136aed05327fd9c1a78df4ef
Roger Florat <rflorat@cidiverte.it>
2018-01-18 10:50:41 +0100

    End of lesson 29.

--- Smack/Smack/Services/SocketService.swift
+++ Smack/Smack/Services/SocketService.swift
@@ -57,7 +57,7 @@ class SocketService: NSObject {
         completion(true)
     }
     
-    func geChatMesseges(completion: @escaping CompletionHandler) {
+    func geChatMesseges(completion: @escaping (_ newMessage: Message) -> Void) {
         
         socket.on("messageCreated") { (dataArray, ack) in
             
@@ -69,17 +69,10 @@ class SocketService: NSObject {
             guard let id = dataArray[6] as? String else { return }
             guard let timeStamp = dataArray[7] as? String else { return }
             
-            if channelId == MessageService.instance.selectedChannel?.id && AuthService.instance.isLoggedIn {
-                
-                let newMessage = Message(message: msgBody, userName: userName, channelId: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timeStamp: timeStamp)
-                
-                MessageService.instance.messages.append(newMessage)
-                
-                completion(true)
-                
-            } else {
-                completion(false)
-            }
+            let newMessage = Message(message: msgBody, userName: userName, channelId: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timeStamp: timeStamp)
+            
+            completion(newMessage)
+            
         }
     }
     
